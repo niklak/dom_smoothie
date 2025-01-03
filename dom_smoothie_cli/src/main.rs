3e6c47b6b62ee21d5a611f17e9470e8fc7aea5c4
Mykola Humanov <morgenpurple@gmail.com>
2025-01-03 10:55:04 +0200

    dom_smoothie_cli/src/main.rs: tool dumps text content and metadata

--- dom_smoothie_cli/src/main.rs
+++ dom_smoothie_cli/src/main.rs
@@ -2,7 +2,8 @@ use std::error::Error;
 use std::{fs, path::PathBuf};
 
 use clap::Parser;
-use dom_smoothie::Readability;
+use dom_smoothie::{Readability, Article};
+
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -18,10 +19,41 @@ struct Cli {
     document_url: Option<String>,
 }
 
+
+/// This struct represents the metadata from the [`dom_smoothie::Article`]
+#[derive(Default, serde::Deserialize, serde::Serialize)]
+struct Metadata {
+    title: String,
+    byline: Option<String>,
+    excerpt: Option<String>,
+    site_name: Option<String>,
+    published_time: Option<String>,
+    modified_time: Option<String>,
+    lang: Option<String>,
+    url: Option<String>,
+    dir: Option<String>,
+}
+
+impl From<&Article> for Metadata {
+    fn from(value: &Article) -> Self {
+        Self {
+            title: value.title.clone(),
+            byline: value.byline.clone(),
+            excerpt: value.excerpt.clone(),
+            site_name: value.site_name.clone(),
+            published_time: value.published_time.clone(),
+            modified_time: value.modified_time.clone(),
+            lang: value.lang.clone(),
+            url: value.url.clone(),
+            dir: value.dir.clone(),
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mut cli = Cli::parse();
 
-    let input_path_buf = cli.input.clone();
+    let input_path_buf = cli.input.with_extension("");
     let Some(input_fn) = input_path_buf.file_name() else {
         return Err("invalid input path".into());
     };
@@ -34,7 +66,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let document_url = cli.document_url.as_deref();
 
     let mut ra = Readability::new(contents, document_url, None)?;
-    let res = ra.parse()?;
+    let article = ra.parse()?;
 
     let Some(output_path) = cli.output else {
         unreachable!();
@@ -44,6 +76,22 @@ fn main() -> Result<(), Box<dyn Error>> {
         .clone()
         .join(format!("{}_result.html", input_fn.to_string_lossy()));
 
-    fs::write(result_html_path, res.content.as_bytes())?;
+    fs::write(result_html_path, article.content.as_bytes())?;
+
+    let result_text_path: PathBuf = output_path
+        .clone()
+        .join(format!("{}_result.txt", input_fn.to_string_lossy()));
+
+    fs::write(result_text_path, article.text_content.as_bytes())?;
+
+    let metadata = Metadata::from(&article);
+    let metadata_content = serde_json::to_string_pretty(&metadata)?;
+
+    let meta_path: PathBuf = output_path
+        .clone()
+        .join(format!("{}_metadata.json", input_fn.to_string_lossy()));
+
+    fs::write(meta_path, metadata_content)?;
+
     Ok(())
 }
