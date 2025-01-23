6dea7c897d2d8d46cc1da053708e835a9cf0fffb
Mykola Humanov <morgenpurple@gmail.com>
2025-01-23 14:11:02 +0200

    Cargo.toml: update

---
+++ tests/metadata.rs
@@ -0,0 +1,23 @@
+use std::fs;
+
+mod common;
+
+use common::test_metadata;
+
+#[test]
+fn test_metadata_last_fail() {
+    test_metadata("./test-pages/ok/arstechnica", Some("http://fakehost/test/"));
+}
+
+#[test]
+fn table_test_metadata() {
+    let source_dirs = ["./test-pages/readability", "./test-pages/ok"];
+    for d in source_dirs {
+        let paths = fs::read_dir(d).unwrap();
+        for p in paths {
+            let pp = p.unwrap().path();
+            println!("path: {:?}", pp);
+            test_metadata(pp, Some("http://fakehost/test/"));
+        }
+    }
+}
