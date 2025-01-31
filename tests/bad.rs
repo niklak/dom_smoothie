4377e143b41b93d9c87e879d8120fed88b741332
Mykola Humanov <morgenpurple@gmail.com>
2025-01-31 14:59:12 +0200

    tests/bad.rs: add `test_skip_body_ancestor_fragment`

--- tests/bad.rs
+++ tests/bad.rs
@@ -27,3 +27,20 @@ fn test_skip_body_ancestor() {
 }
 
 
+#[test]
+fn test_skip_body_ancestor_fragment() {
+    let contents = r#"
+    <div>
+        <p><a class="button" href="https://example.com/sign-up"> Sign Up for Live Updates!</a></p>
+    </div>
+    "#;
+
+    let mut ra = Readability::new(contents, None, None).unwrap();
+    let res = ra.parse().unwrap();
+    let expected: String = r#"<div id="readability-page-1" class="page"><div>
+        <p><a href="https://example.com/sign-up"> Sign Up for Live Updates!</a></p>
+        </div></div>"#.split_whitespace().collect();
+    let got: String = res.content.split_whitespace().collect();
+    assert_eq!(got, expected);
+}
+
