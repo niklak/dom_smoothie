2681a91d18a10aa80e4a44b66079e645f1c378bf
Mykola Humanov <morgenpurple@gmail.com>
2025-04-01 14:34:27 +0300

    src/score.rs: implement `determine_attr_weight` with `aho-corassick` feature

--- src/ac_automat.rs
+++ src/ac_automat.rs
@@ -1,12 +1,14 @@
 use aho_corasick::{AhoCorasick, AhoCorasickKind};
-
 use once_cell::sync::Lazy;
 
-use crate::glob::{MAYBE_CANDIDATES, UNLIKELY_CANDIDATES};
+use crate::glob::{CLASSES_NEGATIVE, CLASSES_POSITIVE, MAYBE_CANDIDATES, UNLIKELY_CANDIDATES};
 
 pub(crate) static AC_UNLIKELY: Lazy<AhoCorasick> = Lazy::new(|| ac_automaton(UNLIKELY_CANDIDATES));
 pub(crate) static AC_MAYBE: Lazy<AhoCorasick> = Lazy::new(|| ac_automaton(MAYBE_CANDIDATES));
-
+pub(crate) static AC_CLASSES_NEGATIVE: Lazy<AhoCorasick> =
+    Lazy::new(|| ac_automaton(CLASSES_NEGATIVE));
+pub(crate) static AC_CLASSES_POSITIVE: Lazy<AhoCorasick> =
+    Lazy::new(|| ac_automaton(CLASSES_POSITIVE));
 
 fn ac_automaton(patterns: &[&str]) -> AhoCorasick {
     AhoCorasick::builder()
@@ -14,5 +16,3 @@ fn ac_automaton(patterns: &[&str]) -> AhoCorasick {
         .build(patterns)
         .unwrap()
 }
-
-
