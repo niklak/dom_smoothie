acf48d4e689069bc596cb2087e40b7804a706c5c
Mykola Humanov <gnk667@proton.me>
2026-09-07 13:16:09 +0300

    src/readability.rs: apply clippy.

--- src/readability.rs
+++ src/readability.rs
@@ -393,15 +393,15 @@ impl Readability {
                 continue;
             };
             let prev_sel = Selection::from(prev_sibling);
-            let prev_img: NodeRef;
-            if prev_sel.is("img") {
-                prev_img = prev_sibling;
+            
+            let prev_img: NodeRef = if prev_sel.is("img") {
+                prev_sibling
             } else if prev_sel.is("*:has( > img:only-child)") {
                 let prev_sel_img = prev_sel.select("img:only-child");
-                prev_img = prev_sel_img.nodes()[0];
+                prev_sel_img.nodes()[0]
             } else {
                 continue;
-            }
+            };
             let noscript_img_sel = Selection::from(*noscript_node).select("img");
             // at this point noscript_img_sel always has one element
             let new_img = &noscript_img_sel.nodes()[0];
